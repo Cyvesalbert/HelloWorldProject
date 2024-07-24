0bccb2a283a7f2e98b77c06116631b1983e2e5d9
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-07-23 21:35:33 -0400

    this is my first commit

--- HelloWorld/src/ht/helloworld/Welcome3.java
+++ HelloWorld/src/ht/helloworld/Welcome3.java
@@ -5,6 +5,7 @@ public class Welcome3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( "Welcome\nto\nJava\nProgramming!" );
-	}
+		System.out.println();
+		}
 
 }
