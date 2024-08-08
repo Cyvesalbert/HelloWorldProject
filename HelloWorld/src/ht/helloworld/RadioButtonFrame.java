0487d590e5510843ee875c2a3cbfb162b58d32cd
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-08 00:50:28 -0400

    test 2

--- HelloWorld/src/ht/helloworld/RadioButtonFrame.java
+++ HelloWorld/src/ht/helloworld/RadioButtonFrame.java
@@ -2,4 +2,19 @@ package ht.helloworld;
 
 public class RadioButtonFrame {
 
+	// Creating radio buttons using ButtonGroup and JRadioButton.
+	3 import java.awt.FlowLayout;
+	4 import java.awt.Font;
+	Fig. 14.19 | JRadioButtons and ButtonGroups. (Part 1 of 3.)
+	578 Chapter 14 GUI Components: Part 1
+	5 import java.awt.event.ItemListener;
+	6 import java.awt.event.ItemEvent;
+	7 import javax.swing.JFrame;
+	8 import javax.swing.JTextField;
+	9 import javax.swing.JRadioButton;
+	10 import javax.swing.ButtonGroup;
+	11
+	12 public class RadioButtonFrame extends JFrame
+	13 {
+	14 private JTextField textField; // used to display font
 }
