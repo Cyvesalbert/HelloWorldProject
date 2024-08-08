24af8bc33cfec5ecbf8c9a9c30c5836ffde03907
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-08 00:42:35 -0400

    test

--- HelloWorld/src/ht/helloworld/TextFieldTest.java
+++ HelloWorld/src/ht/helloworld/TextFieldTest.java
@@ -5,8 +5,6 @@ import javax.swing.JFrame;
 public class TextFieldTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		TextFieldFrame textFieldFrame = new TextFieldFrame();
 		textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		textFieldFrame.setSize(350, 100);
