e4e4a4e025907a483c8141bd10ab1118a7ed9940
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-08 21:57:08 -0400

    test JList Class with single selection capability

---
+++ HelloWorld/src/ht/helloworld/ListTest.java
@@ -0,0 +1,18 @@
+package ht.helloworld;
+
+import javax.swing.JFrame;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ListFrame listFrame = new ListFrame();
+		listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		listFrame.setSize(350, 150);
+		listFrame.setVisible(true);
+		
+		
+	}
+
+}
