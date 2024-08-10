de954bdcedb4c1f9bf899a762eaaf481f405e6df
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-09 20:38:58 -0400

    Handle press of any key

---
+++ HelloWorld/src/ht/helloworld/KeyDemoTest.java
@@ -0,0 +1,15 @@
+package ht.helloworld;
+
+import javax.swing.JFrame;
+
+public class KeyDemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		KeyDemoFrame keyDemoFrame = new KeyDemoFrame();
+		keyDemoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		keyDemoFrame.setSize(350, 100);
+		keyDemoFrame.setVisible(true);
+	}
+
+}
