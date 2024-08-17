ca126e53d90572c862dac2ad18c55c018009c5d6
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-16 23:42:53 -0400

    Choosing colors with JColorChooser

---
+++ HelloWorld/src/ht/helloworld/ShowColors2.java
@@ -0,0 +1,14 @@
+package ht.helloworld;
+
+import javax.swing.JFrame;
+
+public class ShowColors2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ShowColors2JFrame application = new ShowColors2JFrame();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
