12643b8da1325529b7395e56ae5ba6533cad8854
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-08-23 22:13:44 -0400

    testing set of recursive calls for fibonacci

---
+++ HelloWorld/src/ht/helloworld/FibonacciCalculator.java
@@ -0,0 +1,27 @@
+package ht.helloworld;
+
+import java.math.BigInteger;
+
+//Recursive fibonacci method.
+public class FibonacciCalculator {
+
+	private static BigInteger TWO = BigInteger.valueOf(2);
+	
+	// recursive declaration of method fibonacci
+	public static BigInteger fibonacci(BigInteger number) {
+		if(number.equals(BigInteger.ZERO) || number.equals(BigInteger.ONE)) // base cases
+			return number;
+		else // recursion step
+			return fibonacci(number.subtract(BigInteger.ONE)).add(fibonacci(number.subtract(TWO)));
+	}
+	
+	// displays the fibonacci values from 0-40
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int counter = 0; counter <= 40; counter++)
+			System.out.printf("Fibonacci of %d is: %d\n", counter, fibonacci(BigInteger.valueOf(counter)));
+		
+	}
+
+}
