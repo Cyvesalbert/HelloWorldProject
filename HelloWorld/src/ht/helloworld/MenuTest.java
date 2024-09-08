4b4d4309088aa3c9c56a38b43b4ace1901fc6d8e
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-09-08 19:09:55 -0400

    Demonstrating menus

---
+++ HelloWorld/src/ht/helloworld/MenuTest.java
@@ -0,0 +1,18 @@
+package ht.helloworld;
+
+import javax.swing.JFrame;
+
+//Testing MenuFrame.
+public class MenuTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MenuFrame menuFrame = new MenuFrame();
+		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE	);
+		menuFrame.setSize(500, 200);
+		menuFrame.setVisible(true);
+		
+	}
+
+}
