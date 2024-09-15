06cea48eebffbc0b0bdc4819c57facead32e7a02
cyvesalbert <yvesalbertcadet527@gmail.com>
2024-09-15 07:38:37 -0400

    Demonstrating JTabbedPane

---
+++ HelloWorld/src/ht/helloworld/JTabbedPaneDemo.java
@@ -0,0 +1,17 @@
+package ht.helloworld;
+
+import javax.swing.JFrame;
+
+//Demonstrating JTabbedPane
+public class JTabbedPaneDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JTabbedPaneFrame tabbedPaneFrame = new JTabbedPaneFrame();
+		tabbedPaneFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		tabbedPaneFrame.setSize( 250, 200 ); // set frame size
+		tabbedPaneFrame.setVisible( true ); // display frame
+	}
+
+}
