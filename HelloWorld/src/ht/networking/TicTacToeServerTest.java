61d1d63f75ab50ce5a29834eeafd95b766666b95
Cyvesalbert <yvesalbertcadet527@gmail.com>
2024-11-01 11:59:12 -0700

    Server side of client/server Tic-Tac-Toe program

---
+++ HelloWorld/src/ht/networking/TicTacToeServerTest.java
@@ -0,0 +1,16 @@
+package ht.networking;
+
+import javax.swing.JFrame;
+
+//Class that tests Tic-Tac-Toe server.
+public class TicTacToeServerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TicTacToeServer application = new TicTacToeServer();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.execute();
+	}
+
+}
